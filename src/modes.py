708ec73228f47239d31a78ed6bb77216be20e2d5
fortanel <fortanelfr@gmail.com>
2022-11-24 23:09:38 -0600

    Add all my files

---
+++ src/modes.py
@@ -0,0 +1,67 @@
+from .constants import *
+
+class MainMode(object):
+    def __init__(self):
+        self.timer = 0
+        self.scatter()
+        self.freightEnd = False
+
+    def update(self, dt):
+        self.timer += dt
+        if self.timer >= self.time:
+            if self.mode is SCATTER:
+                self.chase()
+            elif self.mode is CHASE:
+                self.scatter()
+
+    def scatter(self):
+        self.mode = SCATTER
+        self.time = 7
+        self.timer = 0
+
+    def chase(self):
+        self.mode = CHASE
+        self.time = 20
+        self.timer = 0
+
+class ModeController(object):
+    def __init__(self, entity):
+        self.timer = 0
+        self.time = None
+        self.mainmode = MainMode()
+        self.current = self.mainmode.mode
+        self.entity = entity 
+
+    def update(self, dt):
+        self.mainmode.update(dt)
+        if self.current is FREIGHT:
+            self.timer += dt
+            if self.timer >= self.time*.75:
+                self.freightEnd = True
+            if self.timer >= self.time:
+                self.time = None
+                self.entity.normalMode()
+                self.current = self.mainmode.mode
+                self.freightEnd = False
+        elif self.current in [SCATTER, CHASE]:
+            self.current = self.mainmode.mode
+
+        if self.current is SPAWN:
+            if self.entity.node == self.entity.spawnNode:
+                self.entity.normalMode()
+                self.current = self.mainmode.mode
+
+    def setSpawnMode(self):
+        if self.current is FREIGHT:
+           self.current = SPAWN
+
+    
+    def setFreightMode(self):
+        if self.current in [SCATTER, CHASE]:
+            self.timer = 0
+            self.time = 7
+            self.current = FREIGHT
+            self.freightEnd = False
+        elif self.current is FREIGHT:
+            self.timer = 0
+            self.freightEnd = False
